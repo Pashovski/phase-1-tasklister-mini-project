186d675319ec694c25aa4cb73797d99fa5f9ac86
Pasha <pashdoctor@gmail.com>
2021-09-06 16:35:08 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+const newTask = document.getElementById('create-task-form')
+const toDoList = document.getElementById('tasks')
+
+newTask.addEventListener('submit', () => {
+  const newText = document.createElement('li')
+  const textInput = document.getElementById('new-task-description')
+  newText.innerText = textInput
+  toDoList.append(newText)
+  event.preventDefault()
+})
